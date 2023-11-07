9fb1ade46744314e3566307f48a3b02338624ec0
lyfjourney <joshua.farara@gmail.com>
2023-11-07 00:52:37 -0500

    Changes to be committed: 	modified:   app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+ 
+# https://hevodata.com/learn/flask-mysql/ 
